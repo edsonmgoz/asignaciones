e7ab3cdfc78b87a3e0f1448a943279a0592ac471
Edson Mollericona Marin <edsonmgoz@gmail.com>
2016-05-13 02:33:19 +0000

    Completando el modulo de tareas

--- src/EMM/UserBundle/Controller/TaskController.php
+++ src/EMM/UserBundle/Controller/TaskController.php
@@ -65,4 +65,104 @@ class TaskController extends Controller
         
         return $this->render('EMMUserBundle:Task:add.html.twig', array('form' => $form->createView()));
     }
+    
+    public function viewAction($id)
+    {
+        $task = $this->getDoctrine()->getRepository('EMMUserBundle:Task')->find($id);
+        
+        if(!$task)
+        {
+            throw $this->createNotFoundException('The task does not exist.');
+        }
+        
+        $deleteForm = $this->createCustomForm($task->getId(), 'DELETE', 'emm_task_delete');
+        
+        $user = $task->getUser();
+        
+        return $this->render('EMMUserBundle:Task:view.html.twig', array('task' => $task, 'user' => $user, 'delete_form' => $deleteForm->createView()));
+    }
+    
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $task = $em->getRepository('EMMUserBundle:Task')->find($id);
+        
+        if(!$task)
+        {
+            throw $this->createNotFoundException('task not found');
+        }
+        
+        $form = $this->createEditForm($task);
+        
+        return $this->render('EMMUserBundle:Task:edit.html.twig', array('task' => $task, 'form' => $form->createView()));
+    }
+    
+    private function createEditForm(Task $entity)
+    {
+        $form = $this->createForm(new TaskType(), $entity, array(
+            'action' => $this->generateUrl('emm_task_update', array('id' => $entity->getId())),
+            'method' => 'PUT'
+        ));
+        
+        return $form;
+    }
+    
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $task = $em->getRepository('EMMUserBundle:Task')->find($id);
+        
+        if(!$task)
+        {
+            throw $this->createNotFoundException('task not found');
+        }
+        
+        $form = $this->createEditForm($task);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() and $form->isValid())
+        {
+            $task->setStatus(0);
+            $em->flush();
+            $this->addFlash('mensaje', 'The task has been modified');
+            return $this->redirectToRoute('emm_task_edit', array('id' => $task->getId()));
+        }
+        
+        return $this->render('EMMUserBundle:Task:edit.html.twig', array('task' => $task, 'form' => $form->createView()));
+    }
+    
+    public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $task = $em->getRepository('EMMUserBundle:Task')->find($id);
+        
+        if(!$task)
+        {
+            throw $this->createNotFoundException('task not found');
+        }
+        
+        $form = $this->createCustomForm($task->getId(), 'DELETE', 'emm_task_delete');
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() and $form->isValid())
+        {
+            $em->remove($task);
+            $em->flush();
+            
+            $this->addFlash('mensaje', 'The task has been deleted');
+            
+            return $this->redirectToRoute('emm_task_index');
+        }
+    }
+    
+    private function createCustomForm($id, $method, $route)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl($route, array('id' => $id)))
+            ->setMethod($method)
+            ->getForm();
+    }
 }
